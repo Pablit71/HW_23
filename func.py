0da4adbac4a2d76d5c6b8e4a3f1fc92634de5d47
Pablo <manbyak5@gmail.com>
2022-08-01 12:43:07 +0300

    edit mypy

--- func.py
+++ func.py
@@ -1,15 +1,23 @@
-def func_query(cmd, value, file_list):
+import re
+from typing import Iterator, List, Any
+
+
+def func_query(cmd: str, value: str, file_list: Iterator) -> List[Any]:
     if cmd == "filter":
-        res = filter(lambda x: (x for x in file_list if value in x), file_list)
+        res = list(filter(lambda x: (x for x in file_list if value in x), file_list))
         return res
     if cmd == "map":
-        value = int(value)
         res = [x.split()[value] for x in file_list]
         return res
     if cmd == 'unique':
         return list(set(value))
     if cmd == 'sort':
-        return sorted(file_list, reverse=False)
+        return list(sorted(file_list, reverse=False))
     if cmd == 'limit':
-        res = list(file_list)[:value]
+        res = list(file_list)[:int(value)]
         return res
+    if cmd == 'regex':
+        compiler = re.compile(value)
+        res = list(filter(lambda x: compiler.search(x), file_list))
+        return res
+    return []
